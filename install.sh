57f003aef4c78566bbb35491dd795631bdf56fc6
Ivan Kucherenko <i.kucherenko@1er.app>
2024-06-26 07:58:34 +0500

    feat: add js linters default config

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ rm -rf ~/.tmux.conf > /dev/null 2>&1
 rm -rf ~/.zshrc > /dev/null 2>&1
 rm -rf ~/.gitconfig > /dev/null 2>&1
 rm -rf ~/.profile > /dev/null 2>&1
+rm -rf ~/package.json > /dev/null 2>&1
+rm -rf ~/package-lock.json > /dev/null 2>&1
+rm -rf ~/eslint.config.js > /dev/null 2>&1
+rm -rf ~/prettier.config.js > /dev/null 2>&1
 
 #==============
 # Create symlinks in the home folder
@@ -22,4 +26,13 @@ ln -sf $dotfiles_dir/.tmux.conf ~/.tmux.conf
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
 ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
 ln -sf $dotfiles_dir/.profile ~/.profile
+ln -sf $dotfiles_dir/package.json ~/package.json
+ln -sf $dotfiles_dir/package-lock.json ~/package-lock.json
+ln -sf $dotfiles_dir/eslint.config.js ~/eslint.config.js
+ln -sf $dotfiles_dir/prettier.config.js ~/prettier.config.js
 
+#==============
+# Configure nodejs dependencies
+#==============
+npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier eslint-plugin-node eslint-config-node
+npx install-peerdeps --dev eslint-config-airbnb
